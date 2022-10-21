9558ae69b10cfe8fdc5ab577f88694206c28d688
Ashokkumar-21 <ashoksulthan21@gmail.com>
2022-10-21 22:12:24 +0530

    [Update]Changes

--- client/src/components/InputText.tsx
+++ client/src/components/InputText.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import './styles/InputText.css'
 
-interface InputTextProps {}
+interface InputTextProps {
+  id?:string;
+  type: 'text' | 'password'
+  label?:string
+}
 
-export const InputText: React.FC<InputTextProps> = ({}) => {
+export const InputText: React.FC<InputTextProps> = ({id,type,label}) => {
   return (
-    <div>
-      <label htmlFor=""></label>
-      <input type="text" />
+    <div className="input-grp-txt">
+      <label htmlFor={id}>{label}</label>
+      <input id={id} type={type} />
     </div>
   );
 };
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,22 +1,30 @@
 import React from "react";
+import { InputText } from "../components/InputText";
 interface HomePageProps {}
 
 export const HomePage: React.FC<HomePageProps> = () => {
   return (
     <div className="main-div">
       <div className="header-div">
-        <h1>Test Page</h1>
+        <h1 className="headerh1">Test Page</h1>
       </div>
       <div className="body-div">
         <div className="body-card">
-          <div className="input-grp-txt">
-            <input type="text" />
+          <h5 className="cardh5">Sign in</h5>
+          <form>
+          <div>
+            <InputText type='text' id="text1" label="Email*" />
           </div>
-          <div className="input-grp-pas">
-            <input type="password" />
+          <div>
+          <InputText type='password' id="text2" label="Password*" />
           </div>
+          </form>
         </div>
       </div>
+      <form>
+        <label htmlFor="lab1">Test</label>
+        <input type="text" id="lab1" />
+      </form>
     </div>
   );
 };

7e41d0403bcfa8960981fa1ce281b93b8971d8c9
Ashokkumar-21 <ashoksulthan21@gmail.com>
2022-10-21 16:47:29 +0530

    [Body Card Add]Commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./styles/style.css"
 import { Approute } from "./routes/Approute";
 
 function App() {
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,14 +1,22 @@
 import React from "react";
-
 interface HomePageProps {}
 
 export const HomePage: React.FC<HomePageProps> = () => {
   return (
-    <div>
-      <h1>Test Page</h1>
-      <div>
-      <input type="text" />
-      </div>      
+    <div className="main-div">
+      <div className="header-div">
+        <h1>Test Page</h1>
+      </div>
+      <div className="body-div">
+        <div className="body-card">
+          <div className="input-grp-txt">
+            <input type="text" />
+          </div>
+          <div className="input-grp-pas">
+            <input type="password" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

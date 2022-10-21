a7a72d4951e386c1f506283b283ac3951d77c718
Ashokkumar-21 <ashoksulthan21@gmail.com>
2022-10-21 16:01:36 +0530

    [2nd]

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -6,7 +6,9 @@ export const HomePage: React.FC<HomePageProps> = () => {
   return (
     <div>
       <h1>Test Page</h1>
+      <div>
       <input type="text" />
+      </div>      
     </div>
   );
 };
